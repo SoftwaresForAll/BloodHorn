ad3e3732df393018137edfb097488fc518e29fff
Unpopular Opinion <solomonsuccessor@gmail.com>
2025-07-28 12:13:19 +0300

    Multiple Quality of life fixes

--- bios/bootsector.asm
+++ bios/bootsector.asm
@@ -20,12 +20,25 @@ start:
 .print_char:
     lodsb
     or al, al
-    jz .load_stage2
+    jz .check_lba
     mov ah, 0x0E
     int 0x10
     jmp .print_char
 
-.load_stage2:
+.check_lba:
+    mov ah, 0x41
+    mov bx, 0x55AA
+    int 0x13
+    jc .use_chs
+    cmp bx, 0xAA55
+    jne .use_chs
+    test cx, 1
+    jz .use_chs
+    jmp .load_lba
+
+.use_chs:
+    mov si, retry_count
+.chs_retry:
     mov ah, 0x02        ; BIOS read sectors
     mov al, 0x01        ; 1 sector
     mov ch, 0x00        ; Cylinder 0
@@ -36,9 +49,32 @@ start:
     mov ax, 0x8000
     mov es, ax
     int 0x13
-    jc .disk_error
+    jc .chs_fail
+    jmp 0x8000:0x0000
 
-    jmp 0x0000:0x8000
+.chs_fail:
+    dec byte [si]
+    jz .disk_error
+    mov ah, 0x00
+    int 0x13
+    jmp .chs_retry
+
+.load_lba:
+    mov si, retry_count
+.lba_retry:
+    mov si, dap
+    mov ah, 0x42
+    mov dl, [BOOT_DRIVE]
+    int 0x13
+    jc .lba_fail
+    jmp 0x8000:0x0000
+
+.lba_fail:
+    dec byte [retry_count]
+    jz .disk_error
+    mov ah, 0x00
+    int 0x13
+    jmp .lba_retry
 
 .disk_error:
     mov si, disk_err
@@ -57,6 +93,15 @@ print_str:
 
 msg db 'BloodHorn BIOS Bootloader', 0
 BOOT_DRIVE db 0
+retry_count db 3
+
+dap:
+    db 0x10               ; DAP size
+    db 0x00               ; Reserved
+    dw 0x0001             ; Number of sectors to read
+    dw 0x0000             ; Offset in ES:BX (0x0000)
+    dw 0x8000             ; Segment in ES:BX (0x8000)
+    dq 0x0000000000000001 ; LBA 1
 
 disk_err db 'Disk Read Error!', 0
 
